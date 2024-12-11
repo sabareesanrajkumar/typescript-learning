91e46941dee85a4b25f2a35f9b813b7663b75c1e
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2024-12-12 04:35:38 +0530

    REST api

---
+++ src/routes/todos.ts
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { Todo } from "../models/todos";
+
+type requestBody = { text: string };
+type requestParams = { todoId: string };
+let todos: Todo[] = [];
+const router = Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({ todos: todos });
+});
+
+router.post("/todo", (req, res, next) => {
+  const body = req.body as requestBody;
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: body.text,
+  };
+
+  todos.push(newTodo);
+  res.status(201).json({ message: "posted" });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const params = req.params as requestParams;
+  const tid = params.todoId;
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    res.status(200).json({ message: "updated" });
+  }
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  const params = req.params as requestParams;
+  todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
+  res.status(200).json({ message: "deleted" });
+});
+
+export default router;
